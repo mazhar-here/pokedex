e0952bc510e7dcc49891f35c11aac986a3e1e127
mazhar-here <mazhar_live@hotmail.com>
2021-06-04 00:59:04 +0200

    Initialize project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+
+const template = <p>ASSD ASDSAD from react</p>;
+
+ReactDOM.render(template, document.getElementById('root'));
